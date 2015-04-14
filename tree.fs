6b169b76a13a3fa7862df68e805bf3354cd759a9
Matt <mroll1729@gmail.com>
2015-04-14 19:31:43 -0400

    added higher order traversal to tree.fs

--- tree.fs
+++ tree.fs
@@ -1,10 +1,37 @@
+( Short note on word api's:
+    n - integer
+    adr - variable address
+    n_adr - node address
+    r_adr - root address
+    c_adr - code address [for delivery to EXECUTE
+)
+
 : NODE  HERE 24 ALLOT DUP DUP 24 0 FILL ROT SWAP ! ; ( n -- adr )
 : LCHILD   8 + ; ( n_adr -- lc_adr )
 : RCHILD   16 + ; ( n_adr -- rc_adr )
-: NINSERT   RECURSIVE DUP @ 0= IF ! ELSE 2DUP @ @ SWAP @ > IF @ LCHILD NINSERT ELSE @ RCHILD NINSERT THEN THEN ; ( n_adr r_adr -- )
-: NSEARCH   RECURSIVE DUP @ 0= IF DROP DROP 0 ELSE 2DUP @ @ = IF 1 ELSE 2DUP @ @ < IF @ LCHILD NSEARCH ELSE @ RCHILD NSEARCH THEN THEN THEN ; ( n r_adr -- f )
-: NFIND   RECURSIVE DUP @ 0= IF DROP DROP 0 ELSE 2DUP @ @ = IF @ ELSE 2DUP @ @ < IF @ LCHILD NFIND ELSE @ RCHILD NFIND THEN THEN THEN ; ( n r_adr -- n_adr [0 if not found] )
-: PREORDER   RECURSIVE DUP @ 0= IF DROP ELSE DUP @ @ . DUP @ LCHILD PREORDER @ RCHILD PREORDER THEN ; ( r_adr -- )
+: NINSERT   RECURSIVE DUP @ 0= IF ! ELSE
+                2DUP @ @ SWAP @ > IF @ LCHILD NINSERT ELSE
+                @ RCHILD NINSERT THEN THEN ; ( n_adr r_adr -- )
+
+: NSEARCH   RECURSIVE DUP @ 0= IF DROP DROP 0 ELSE
+                2DUP @ @ = IF 1 ELSE
+                2DUP @ @ < IF @ LCHILD NSEARCH ELSE
+                @ RCHILD NSEARCH THEN THEN THEN ; ( n r_adr -- f )
+
+: NFIND   RECURSIVE DUP @ 0= IF DROP DROP 0 ELSE
+                2DUP @ @ = IF @ ELSE
+                2DUP @ @ < IF @ LCHILD NFIND ELSE
+                @ RCHILD NFIND THEN THEN THEN ; ( n r_adr -- n_adr [0 if not found] )
+
+: PRETRAVERSE   RECURSIVE DUP @ 0= IF DROP DROP ELSE
+                    2DUP @ SWAP EXECUTE 2DUP @ LCHILD PRETRAVERSE @ RCHILD PRETRAVERSE THEN ; ( c_adr r_adr -- )
+
+: NPRINT   @ . CR ; ( n_adr -- )
+VARIABLE 'PT
+: PT   'PT @ ; ( -- )
+' NPRINT 'PT !
+
+: PREORDER   PT SWAP PRETRAVERSE ;
 
 
 VARIABLE NROOT
@@ -17,4 +44,4 @@ VARIABLE NROOT
 10 NODE NROOT NINSERT
 6 NODE NROOT NINSERT
 
-NROOT PREORDER CR
+NROOT PREORDER
