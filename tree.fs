6f98e66de3798de21049ab2e4c72327186dda472
Matt <mroll1729@gmail.com>
2015-04-26 15:51:17 -0400

    implemented balanced deletion in tree.fs

--- tree.fs
+++ tree.fs
@@ -3,7 +3,7 @@
     f - result of a boolean expression
     adr - variable address
     n_adr - node address
-    r_adr - root address
+    r_adr - pointer to root node address
     c_adr - code address [for delivery to EXECUTE]
 )
 
@@ -37,82 +37,147 @@
 
 : NOT0   0= 0= ; ( n -- f )
 
-: SKEW   RECURSIVE DUP @ LVL NOT0 IF DUP DUP @ DUP HAS_LC IF LVL SWAP @ LCHILD @ LVL = IF DUP RROTATE ELSE
-                THEN @ RCHILD SKEW ELSE DROP DROP DROP THEN ELSE
-                DROP THEN ; ( r_adr -- )
+: @LC_LVL   LCHILD @ LVL ; ( n_adr -- lc_lvl )
 
-: SPLIT  RECURSIVE DUP NULL? IF DROP ELSE
-                DUP @ HAS_RC IF DUP @ RCHILD @ HAS_RC IF DUP DUP @ RCHILD @ LVL SWAP @ LVL DUP NOT0 IF = IF DUP LROTATE SPLIT ELSE
-                DROP THEN ELSE
-                DROP THEN ELSE
-                DROP THEN ELSE
-                DROP THEN THEN ; ( r_adr -- )
+: @RC_LVL   RCHILD @ LVL ; ( n_adr -- rc_lvl )
 
-: HEIGHT   RECURSIVE DUP NULL? IF DROP -1 ELSE
-                DUP 1 SWAP @ RCHILD HEIGHT + SWAP 1 SWAP @ LCHILD HEIGHT + MAX THEN ; ( r_adr -- h )
+: DEC_LVL   DUP LVL 1 - SWAP SET_LVL ; ( n_adr -- )
 
-: AINSERT   RECURSIVE DUP NULL? IF SWAP DUP 1 SWAP SET_LVL SWAP ! ELSE
-                2DUP @ @ SWAP @ > IF TUCK TUCK @ LCHILD AINSERT ELSE
-                TUCK TUCK @ RCHILD AINSERT THEN SKEW SPLIT THEN ; ( n_adr r_adr -- )
+: SKEW   RECURSIVE DUP XNULL?
+                IF DUP @ LVL NOT0
+                    IF DUP DUP @ DUP HAS_LC
+                        IF LVL SWAP @ @LC_LVL =
+                            IF DUP RROTATE
+                            THEN @ RCHILD SKEW
+                        ELSE DROP DROP DROP
+                        THEN
+                    ELSE DROP
+                    THEN
+                ELSE DROP
+                THEN ; ( r_adr -- )
 
-: ADELETE   RECURSIVE DUP XNULL?
-                                IF 2DUP @ @ =
-                                    IF DUP DUP @ LCHILD NULL? SWAP @ RCHILD NULL? + 0=
-                                        IF DUP @ LCHILD @ SAVE
-                                            BEGIN
-                                                DUP @ RCHILD XNULL?
-                                                IF DUP DUP @ RCHILD @ SWAP ! 0
-                                                ELSE 1
-                                                THEN
-                                            UNTIL ROT DROP @ @ OVER @ ! DUP @ @ SWAP @ LCHILD ADELETE
-                                        ELSE DUP @ HAS_LC
-                                            IF SWAP DROP DUP @ LCHILD @ SWAP !
-                                            ELSE SWAP DROP DUP @ RCHILD @ SWAP !
-                                            THEN
-                                        THEN
-                                    ELSE 2DUP @ @ <
-                                        IF @ LCHILD
-                                        ELSE @ RCHILD
-                                        THEN ADELETE
-                                    THEN
-                                 THEN ; ( n r_adr -- )
+: SPLIT  RECURSIVE DUP XNULL?
+                IF DUP @ HAS_RC
+                    IF DUP @ RCHILD @ HAS_RC
+                        IF DUP DUP @ RCHILD @ @RC_LVL SWAP @ LVL DUP NOT0
+                            IF =
+                                IF DUP LROTATE SPLIT
+                                ELSE DROP
+                                THEN
+                            ELSE DROP
+                            THEN
+                        ELSE DROP
+                        THEN
+                    ELSE DROP
+                    THEN
+                ELSE DROP
+                THEN ; ( r_adr -- )
 
+: HEIGHT   RECURSIVE DUP NULL?
+                IF DROP -1
+                ELSE DUP 1 SWAP @ RCHILD HEIGHT + SWAP 1 SWAP @ LCHILD HEIGHT + MAX
+                THEN ; ( r_adr -- h )
 
-: NSEARCH   RECURSIVE DUP NULL? IF DROP DROP 0 ELSE
-                2DUP @ @ = IF 1 ELSE
-                2DUP @ @ < IF @ LCHILD NSEARCH ELSE
-                @ RCHILD NSEARCH THEN THEN THEN ; ( n r_adr -- f )
+: AINSERT   RECURSIVE DUP NULL?
+                IF SWAP DUP 1 SWAP SET_LVL SWAP !
+                ELSE 2DUP @ @ SWAP @ >
+                    IF TUCK TUCK @ LCHILD AINSERT
+                    ELSE TUCK TUCK @ RCHILD AINSERT
+                    THEN SKEW SPLIT
+                THEN ; ( n_adr r_adr -- )
 
-: NFIND   RECURSIVE DUP NULL? IF DROP DROP 0 ELSE
-                2DUP @ @ = IF @ ELSE
-                2DUP @ @ < IF @ LCHILD NFIND ELSE
-                @ RCHILD NFIND THEN THEN THEN ; ( n r_adr -- n_adr [0 if not found] )
-
-: PRETRAVERSE   RECURSIVE DUP NULL? IF DROP DROP ELSE
-                    2DUP @ SWAP EXECUTE 2DUP @ LCHILD PRETRAVERSE @ RCHILD PRETRAVERSE THEN ; ( c_adr r_adr -- )
-
-: NPRINT   @ . CR ; ( n_adr -- )
+: NPRINT   DUP LVL SWAP @ . ." ," . CR ; ( n_adr -- )
 
 VARIABLE 'PT
 
-' NPRINT 'PT !
+: NPRINT 'PT !
 
 : PT   'PT @ ; ( -- xt. puts execution token of NPRINT on the stack )
 
+: PRETRAVERSE   RECURSIVE DUP NULL?
+                    IF DROP DROP
+                    ELSE 2DUP @ SWAP EXECUTE 2DUP @ LCHILD PRETRAVERSE @ RCHILD PRETRAVERSE
+                    THEN ; ( c_adr r_adr -- )
+
 : PREORDER   PT SWAP PRETRAVERSE ; ( r_adr -- )
 
+
+: BALANCE   DUP XNULL?
+                IF DUP @ HAS_LC
+                    IF DUP @ @LC_LVL
+                    ELSE 0
+                    THEN OVER @ LVL 1 - < OVER DUP DUP @ HAS_RC
+                    IF @ @RC_LVL
+                    ELSE DROP 0
+                    THEN SWAP @ LVL 1 - < + NOT0
+                    IF DUP @ DEC_LVL DUP @ HAS_RC
+                        IF DUP @ @RC_LVL
+                        ELSE 0
+                        THEN OVER @ LVL >
+                        IF DUP DUP @ RCHILD @ SWAP @ LVL SWAP SET_LVL
+                        THEN DUP SKEW SPLIT
+                    ELSE DROP
+                    THEN
+                ELSE DROP
+                THEN ; ( r_adr -- )
+
+: ADELETE   RECURSIVE TUCK DUP XNULL?
+                IF 2DUP @ @ =
+                    IF DUP DUP @ LCHILD NULL? SWAP @ RCHILD NULL? + 0=
+                        IF DUP @ LCHILD @ SAVE
+                            BEGIN
+                                DUP @ RCHILD XNULL?
+                                IF DUP DUP @ RCHILD @ SWAP ! 0
+                                ELSE 1
+                                THEN
+                            UNTIL ROT DROP @ @ OVER @ ! DUP @ @ SWAP @ LCHILD ADELETE
+                        ELSE DUP @ HAS_LC
+                            IF SWAP DROP DUP @ LCHILD @ SWAP !
+                            ELSE SWAP DROP DUP @ RCHILD @ SWAP !
+                            THEN
+                        THEN
+                    ELSE 2DUP @ @ <
+                        IF @ LCHILD
+                        ELSE @ RCHILD
+                        THEN ADELETE
+                    THEN 
+                 THEN BALANCE ; ( n r_adr -- )
+
+: NSEARCH   RECURSIVE DUP NULL?
+                IF DROP DROP 0
+                ELSE 2DUP @ @ =
+                    IF 1
+                    ELSE 2DUP @ @ <
+                        IF @ LCHILD NSEARCH
+                        ELSE @ RCHILD NSEARCH
+                        THEN
+                    THEN
+                THEN ; ( n r_adr -- f )
+
+: NFIND   RECURSIVE DUP NULL?
+                IF DROP DROP 0
+                ELSE 2DUP @ @ =
+                    IF @
+                    ELSE 2DUP @ @ <
+                        IF @ LCHILD NFIND
+                        ELSE @ RCHILD NFIND
+                        THEN
+                    THEN
+                THEN ; ( n r_adr -- n_adr [0 if not found] )
+
+
 VARIABLE NROOT
+0 NODE NROOT AINSERT
 1 NODE NROOT AINSERT
 2 NODE NROOT AINSERT
 3 NODE NROOT AINSERT
-4 NODE NROOT AINSERT
 5 NODE NROOT AINSERT
-NROOT PREORDER
+4 NODE NROOT AINSERT
+6 NODE NROOT AINSERT
 
-2 NROOT ADELETE
-CR
-NROOT PREORDER
-4 NROOT ADELETE
-CR
-NROOT PREORDER
+0 NROOT ADELETE
+3 NROOT ADELETE
+1 NROOT ADELETE
+NROOT PREORDER CR
+NROOT HEIGHT
 .S CR
